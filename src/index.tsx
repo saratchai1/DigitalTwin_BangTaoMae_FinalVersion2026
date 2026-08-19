5965fc9ef278f77f236457b281484dcca096f76b
saratchai1 <saratchai@gmail.com>
2026-08-19 22:07:20 +0700

    Load world-class UI design layer

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import "./index.css";
+import "./ui-overrides.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
